34b4b81a044c6686a7f2b73641b6ab1582e026fd
Shaukatjassme <shaukat.jassme786@gmail.com>
2024-07-07 17:33:46 +0500

    updated-7

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import { useState } from "react"
+import bird  from "./svg/bird.svg"
+import cat  from "./svg/cat.svg"
+import cow  from "./svg/cow.svg"
+import dog  from "./svg/dog.svg"
+import gator  from "./svg/gator.svg"
+import heart from "./svg/heart.svg"
+import horse from "./svg/horse.svg"
+
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}
+
 
 function AnimalShow({ type }) {
+
+    
+  const [clicks, setClicks] = useState(0);
+
+  function handleClick() {
+  
+    setClicks(clicks + 1);
+  
+  }
+  
+
       return (
-          <div>
-              <h1>{type}</h1>
-          </div>
+        <div onClick={handleClick} className="image" style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={svgMap[type]} className="heart" alt="" style={{ width: '50px' }} />
+        <img src={heart} className="heart2" alt="" style={{ width: `${10 + 10 * clicks}px`, marginLeft: '10px' }} />
+      </div>
       )
 }
 
